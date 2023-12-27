a82a1914e84cafa3a25460b04a608fbe0830b60f
gtkymn <154983689+gtkymn@users.noreply.github.com>
2023-12-27 13:37:10 +0000

    Update export.sh

--- main/config/export.sh
+++ main/config/export.sh
@@ -3,7 +3,7 @@
 export PORT=${PORT:-80}
 export USERNAME=${USERNAME:-admin}
 export PASSWORD=${PASSWORD:-admin}
-export UUID=${UUID:-1eb6e917-774b-4a84-aff6-b058577c60a5}
+export UUID=${UUID:-e8dbe899-2dd6-4328-9da8-d79b67e57401}
 export PATH_VLESS=${PATH_VLESS:-/$UUID/vless}
 export PATH_VMESS=${PATH_VMESS:-/$UUID/vmess}
 export WARP_SERVER=${WARP_SERVER:-engage.cloudflareclient.com}
